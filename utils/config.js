a44c75370024e7eba93beef1580a2101c494d2ed
Mayra <mayra.pastor@est.fib.upc.edu>
2021-03-19 18:35:45 +0100

    Firebase and cheers sound

---
+++ utils/config.js
@@ -0,0 +1,9 @@
+export var firebaseConfig = {
+    apiKey: "AIzaSyD4DU5J3430TgsNvo20Vzadeo27HTXB6lI",
+    authDomain: "poseman-914be.firebaseapp.com",
+    databaseURL: "https://poseman-914be-default-rtdb.firebaseio.com",
+    projectId: "poseman-914be",
+    storageBucket: "poseman-914be.appspot.com",
+    messagingSenderId: "169732553726",
+    appId: "1:169732553726:web:cbbef27440d22480775eaf"
+};
--- utils/timer.js
+++ utils/timer.js
@@ -1,4 +1,9 @@
 import {TimeCounter, Timer} from "easytimer.js"
+import {sound} from  '../camera.js';
+import {soundBackground} from  './gameUtils.js';
+import {firebaseConfig} from './config.js'
+import firebase from "firebase/app";
+import "firebase/database";
 
 var timer = new Timer();
 const easy = 10;
@@ -6,7 +11,8 @@ const medium = 20;
 const hard = 15;
 var timer_div = document.getElementById("timer");
 
-
+var soundCheers = new sound('./sounds/cheers.mp3');
+var soundGameOver = new sound('./sounds/gameOver.mp3');
 
 export const countDown = function(){
     timer.start({countdown:true,precision:'secondTenths',startValues:{seconds:easy}});
@@ -15,11 +21,10 @@ export const countDown = function(){
         console.log(`time:${timer.getTimeValues().toString()}`)
     });
 
-
     timer.addEventListener('targetAchieved',function(e){
         timer_div.innerHTML = "Final Score is : 100";
         // console.log("INVOKE GAMEOVER HERE");
-        // gameOver();
+        gameOver();
     })
 };
 
@@ -27,12 +32,25 @@ export const countDown = function(){
 
 export const gameOver = function(){
     document.getElementById("start_words").style.visibility = "hidden";
-
+    soundBackground.stop();
+    soundCheers.play();
     document.getElementById("right_game_window_h1").innerHTML = "Game Over!";
     document.getElementById("right_game_window_p").innerHTML ="Your all streached and ready for next round of coding. Good luck Soldier :)";
     document.getElementById("guess_image").style.visibility = "hidden";
     document.getElementById("word_list").style.visibility = "hidden"
     document.getElementById("replay_btn").style.visibility = "visible";
-
+    
+    //Save Score to Firebase - Need to change
+    saveScore("Net", 10);
 }
 
+function saveScore(userName, scoreDone) {
+    // Initialize Firebase
+    var x =  Math.floor(Math.random()*100);
+    firebase.initializeApp(firebaseConfig);
+    var database = firebase.database();
+    firebase.database().ref('users/' + 'user'+x).set({
+        name: userName,
+        score: scoreDone
+      });
+}
